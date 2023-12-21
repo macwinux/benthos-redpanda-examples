af6531f062f9b3dedd56075f710c3431b81e02a0
Martínez Carreño, Carlos <cmcarreno@indra.es>
2023-12-21 18:15:05 +0100

    two examples using benthos and redpanda

---
+++ redpanda-schema-registry/insert-schema.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+
+schema = {
+    "type": "record",
+    "name": "BenthosExample",
+    "fields": [
+      { "name": "ID", "type": "string" },
+      { "name": "Name", "type": "string" },
+      { "name": "Gooeyness", "type": "double", "default": 0 },
+#      { "name": "Bouncing", "type": "boolean", "default": True }
+    ]
+}
+base_uri = "http://localhost:8081"
+
+def pretty(text):
+  print(json.dumps(text, indent=2))
+  
+res = requests.post(
+    url=f'{base_uri}/subjects/benthos_example/versions',
+    data=json.dumps({
+      'schema': json.dumps(schema)
+    }),
+    headers={'Content-Type': 'application/vnd.schemaregistry.v1+json'}).json()
+pretty(res)
+
+
